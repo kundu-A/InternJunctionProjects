ba4b225ab409d1a77dccc1a88ef4f1282a221808
Kundu-A <kunduarpan43@gmail.com>
2024-07-20 16:55:34 +0530

    thrid commit

--- TIC-TAC-TOE/TicTacToe.java
+++ TIC-TAC-TOE/TicTacToe.java
@@ -98,10 +98,12 @@ class GameAlgorithm{
     {
         if((board[0][0]=='X' && board[1][0]=='X' && board[2][0]=='X') || (board[0][0]=='X' && board[0][1]=='X' && board[0][2]=='X') || (board[0][1]=='X' && board[1][1]=='X' && board[2][1]=='X') || (board[1][0]=='X' && board[1][1]=='X' && board[1][2]=='X') || (board[0][2]=='X' && board[1][2]=='X' && board[2][2]=='X') || (board[2][0]=='X' && board[2][1]=='X' && board[2][2]=='X')){
             System.out.println(Xuser+" won the match");
+            System.out.println(Ouser+" loss the match");
             System.exit(0);
         }
         else if((board[0][0]=='O' && board[1][0]=='O' && board[2][0]=='O') || (board[0][0]=='O' && board[0][1]=='O' && board[0][2]=='O') || (board[0][1]=='O' && board[1][1]=='O' && board[2][1]=='O') || (board[1][0]=='O' && board[1][1]=='O' && board[1][2]=='O') || (board[0][2]=='O' && board[1][2]=='O' && board[2][2]=='O') || (board[2][0]=='O' && board[2][1]=='O' && board[2][2]=='O')){
             System.out.println(Ouser+" won the match.");
+            System.out.println(Xuser+" loss the match");
             System.exit(0);
         }
         else{
